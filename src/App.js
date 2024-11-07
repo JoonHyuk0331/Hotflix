9b8fc3abf17b7fd2691df363906f6972af629ad9
JoonHyuk0331 <joonlife0901@gmail.com>
2024-11-07 23:07:45 +0900

    feat(App.js):최소 4개 이상의 TMDB API를 사용하여 영화리스트 호출

--- src/App.js
+++ src/App.js
@@ -47,15 +47,61 @@ const Header = ({ isLoggedIn, username, onLogout }) => {
 
 // 메인 화면 컴포넌트
 const Main = () => {
-  const [movies, setMovies] = useState([]);
+  const [now_playing_movies, set_now_playing_Movies] = useState([]);
+  const [popular_movies, set_popular_Movies] = useState([]);
+  const [toprated_movies, set_toprated_Movies] = useState([]);
+  const [upcomming_movies, set_upcoming_Movies] = useState([]);
 
+  //set_now_playing_Movies
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR&api_key=8cc1274fb9b4939dd84d9741f37e166e`);
         const data = await response.json();
         console.log(data);
-        setMovies(data.results); // 영화 목록 중 4개 항목만 가져오기 data.results.slice(0, 4)
+        set_now_playing_Movies(data.results); // 영화 목록 중 4개 항목만 가져오기 data.results.slice(0, 4)
+      } catch (error) {
+        console.error("Failed to fetch movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+  //set_popular_Movies
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?language=ko&page=1&region=KR&api_key=8cc1274fb9b4939dd84d9741f37e166e`);
+        const data = await response.json();
+        console.log(data);
+        set_popular_Movies(data.results); // 영화 목록 중 4개 항목만 가져오기 data.results.slice(0, 4)
+      } catch (error) {
+        console.error("Failed to fetch movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+  //set_toprated_Movies
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1&region=KR&api_key=8cc1274fb9b4939dd84d9741f37e166e`);
+        const data = await response.json();
+        console.log(data);
+        set_toprated_Movies(data.results); // 영화 목록 중 4개 항목만 가져오기 data.results.slice(0, 4)
+      } catch (error) {
+        console.error("Failed to fetch movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+  //set_upcoming_Movies
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?language=ko&page=1&region=KR&api_key=8cc1274fb9b4939dd84d9741f37e166e`);
+        const data = await response.json();
+        console.log(data);
+        set_upcoming_Movies(data.results); // 영화 목록 중 4개 항목만 가져오기 data.results.slice(0, 4)
       } catch (error) {
         console.error("Failed to fetch movies:", error);
       }
@@ -66,25 +112,47 @@ const Main = () => {
   return (
     <main>
     
-      <h1>Popular Movies</h1>
-      <div className="movies-container">
-        {movies.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <h2>{movie.title}</h2>
-          </div>
-        ))}
-      </div>
-      <h1>한국 상영중 영화</h1>
+      <h1>현재 상영중 영화</h1>
       <div className="movies-container">
         {
-          movies.map((movie)=>{
+          now_playing_movies.map((movie)=>{
             return(
               <Movie key={movie.id} movie={movie} />
             )
           })
         }
       </div>
+      <h1>인기있는 영화</h1>
+      <div className="movies-container">
+        {
+          popular_movies.map((movie)=>{
+            return(
+              <Movie key={movie.id} movie={movie} />
+            )
+          })
+        }
+      </div>
+      <h1>평점이 높은 영화</h1>
+      <div className="movies-container">
+        {
+          toprated_movies.map((movie)=>{
+            return(
+              <Movie key={movie.id} movie={movie} />
+            )
+          })
+        }
+      </div>
+      <h1>개봉 예정 영화</h1>
+      <div className="movies-container">
+        {
+          upcomming_movies.map((movie)=>{
+            return(
+              <Movie key={movie.id} movie={movie} />
+            )
+          })
+        }
+      </div>
+
     </main>
   );
 };
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,16 +3,13 @@ import React, { useState, useEffect } from "react";
 // Movie 컴포넌트
 export default function Movie({ movie }) {
   return (
-    <div className="movie-container">
-      <img 
-        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} 
-        alt={movie.title} 
-      />
-      <div className="movie-info">
-        <h4>{movie.title}</h4>
-        <span>{movie.overview}</span>
-        <span>평점: {movie.vote_average}</span>
-      </div>
+    <div className="movies-container">
+        <div className="movie-card">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <h4>{movie.title}</h4>
+            <span>평점:{movie.vote_average}</span>
+        </div>
     </div>
   );
-}
+}   
+//alt는 로딩창으로 쓰면됨
